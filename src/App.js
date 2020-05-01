1bb30217455e2f596e817d9fdeae4f120bcbdbc6
Surya Vedantam <surya.phani1729@gmail.com>
2020-05-01 17:24:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ChatComponent from './components/chant/ChantComponent';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <ChatComponent />
+          </Route>
+          <Route path="/about">
+            <ChatComponent />
+          </Route>
+          <Route path="/dashboard">
+            <ChatComponent />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/chant/ChantComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles';
+
+function ChantComponent() {
+  return (
+    <div className="App">
+       <input type="date" /><br />
+       <input type="number" /><br/>
+       <input type="button" value="submit" />
+    </div>
+  );
+}
+
+export default ChantComponent;
